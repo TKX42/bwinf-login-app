7712246228c2988680ca0895e28cce575903657d
Benno <bennokolar@gmail.com>
2024-03-21 14:45:04 +0100

    added daisyUI

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,10 +2,10 @@
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold mx-auto p-4">
-      You are logged in
-    </h1>
+  <h1 class="text-3xl font-bold mx-auto p-4">
+    You are logged in
+    <button class="btn btn-primary">Hello</button>
+  </h1>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
